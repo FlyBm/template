2e09ff512ebbcd9416d7043bf703fa368d3a2eff
kzime <kzime@qq.com>
2021-10-31 16:03:56 +0800

    support mkdocs.yml generate

--- split.py
+++ split.py
@@ -1,6 +1,10 @@
 import os
 import shutil
 
+config = '''
+nav:
+'''
+
 for root, dirs, files in os.walk(".", topdown=False):
   if root != '.': continue
   # print(files)
@@ -14,27 +18,32 @@ for root, dirs, files in os.walk(".", topdown=False):
   for file in files:
     if '.md' not in file: continue
     if 'README' in file: continue
-    print(file)
+    # print(file)
     shutil.copy(file, 'tmp/' + file)
-    # with open('./tmp/' + file, 'r', encoding='utf-8') as f:
       
-
     with open('./tmp/' + file, 'r', encoding='utf-8') as f:
-      # print(f.read().split('### '))
       lst = f.read().split('\n' + r'### ')
-      # print(len(lst))
       folder = lst[0].split('\n')[0]
       while folder[0] in ['#', ' ']: 
         folder = folder[1:]
-      # print(folder)
       # folder name done
-      
+
       os.mkdir('tmp/' + folder)
+      config += '  - ' + folder + ':\n'
+
       for str in lst[1:]:
-        filename = str.split('\n')[0]
-        # os.
+        filename = str.split('\n')[0].strip()
         with open('tmp/' + folder + '/' + filename + '.md', 'w', encoding='utf-8') as w:
           w.write(str)
         # print(filename)
         # print('---'*10)
-    # break
+        config += '    - ' + 'tmp/' + folder + '/' + filename + '.md\n'
+    os.remove('tmp/' + file)
+  # config done
+
+  print('All clear')
+  print('将tmp/ 放入OI-wiki/docs\n将tmp/mkdocs.yml内容覆盖OI-wiki/mkdocs.yml')
+  with open('tmp/mkdocs.yml', 'w', encoding='utf-8') as w:
+    w.write(config)
+  
+
